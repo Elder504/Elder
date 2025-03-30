3be164d13815e50e255cd26913525503d7c46cd2
Ian.xyz <taiwam34@gmail.com>
2025-03-30 11:35:22 -0300

    |

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -1,15 +1,18 @@
-import {WAMessageStubType} from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
+import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
 
-export async function before(m, {conn, participants, groupMetadata}) {
+export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => welcome)
-  let img = await (await fetch(`${pp}`)).buffer()
-  
-  let chat = global.db.data.chats[m.chat]
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://static.wikia.nocookie.net/yugioh/images/f/f7/Card_Back.jpg/revision/latest?cb=20120521235147&path-prefix=es');
+  let img = await (await fetch(pp)).buffer();
 
- if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `*Hola ✦ @${m.messageStubParameters[0].split`@`[0]}*\n\n${global.welcome}`
-await conn.sendMini(m.chat, redes, dev, welcome, img, img, redeshost)
+  let chat = global.db.data.chats[m.chat];
+
+  if (chat.welcome && m.messageStubType == 27) {
+    let welcome = global.welcome
+      .replace('+tag', `@${m.messageStubParameters[0].split('@')[0]}`)
+      .replace('+description', groupMetadata.desc || 'Sin descripción');
+
+    await conn.sendMini(m.chat, redes, dev, welcome, img, img, redeshost);
   }
 }
