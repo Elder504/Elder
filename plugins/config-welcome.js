c4e78f9d56d2c067d47598eceff602e80bc52f08
Ian.xyz <taiwam34@gmail.com>
2025-03-30 12:10:17 -0300

    |

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -16,7 +16,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 800, height: 400, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .resize({ width: 600, height: 300, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
         .toBuffer();
 
       await conn.sendMini(m.chat, redes, dev, welcome, extendedImage, extendedImage, redeshost);
