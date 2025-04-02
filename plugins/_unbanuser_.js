4f3c6c17933b0cdb23c9c6dd1801b31af3fdd752
Elder504 <elderposadas028@gmail.com>
2025-04-02 10:16:17 -0600

    Actualizar _unbanuser_.js

--- plugins/_unbanuser_.js
+++ plugins/_unbanuser_.js
@@ -1,23 +1,27 @@
-let handler = async (m, { conn, text }) => {
-    let who;
-
-    if (text.includes('@')) {
-        who = text.split('@')[1] + '@s.whatsapp.net'; 
+const handler = async (m, { conn, args, text, usedPrefix, command }) => {
+    let user;
+    let db = global.db.data.users;
+    if (m.quoted) {
+        user = m.quoted.sender;
+    } else if (args.length >= 1) {
+        user = args[0].replace('@', '') + '@s.whatsapp.net';
     } else {
-        who = `${text}@s.whatsapp.net`; 
+        await conn.reply(m.chat, `🚩 Etiqueta o responde al mensaje del usuario que quieras Desbanear, Ejemplo:\n> → *${usedPrefix}unbanuser <@tag>*`, m);
+        return;
     }
-
-    let users = global.db.data.users;
-
-    if (users[who] && users[who].banned) {
-        users[who].banned = false;
-        conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} ha sido desbaneado*`, fkontak, { mentions: [who] });
+    if (db[user]) {
+        db[user].banned = false;
+        db[user].banRazon = '';
+        const nametag = await conn.getName(user);
+        const nn = conn.getName(m.sender);
+        await conn.reply(m.chat, `✅ ᴇʟ ᴜsᴜᴀʀɪᴏ *${nametag}* ʜᴀ sɪᴅᴏ ᴅᴇsʙᴀɴᴇᴀᴅᴏ.`, m, { mentionedJid: [user] });
     } else {
-        conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} no está baneado*`, fkontak, { mentions: [who] });
+        await conn.reply(m.chat, `🚩 El usuario no está registrado.`, m);
     }
 };
-
+handler.help = ['unbanuser <@tag>'];
 handler.command = ['unbanuser'];
-handler.rowner = true;
-
+handler.tags = ['owner'];
+handler.owner = true;
+handler.group = true;
 export default handler;
