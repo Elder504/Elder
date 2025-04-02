2506db41e5fd7985c5e46b2b8e835492896da9c9
Elder504 <elderposadas028@gmail.com>
2025-04-02 15:15:03 -0600

    Actualizar chat_unbanchat.js

--- plugins/chat_unbanchat.js
+++ plugins/chat_unbanchat.js
@@ -1,14 +1,15 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '*Este chat no se encuentra registrado*', m, fake)
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *¡Este chat no está registrado!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '*El bot fue desactivado en este chat*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '[🌠] *El bot no está baneado en este chat*', m)
 chat.isBanned = false
-await conn.reply(m.chat, '*El bot ha sido reactivado*', m, fake)
+await conn.reply(m.chat, `➩ *ᴇʟ ʙᴏᴛ ʜᴀ sɪᴅᴏ ᴀᴄᴛɪᴠᴀᴅᴏ ᴇɴ ᴇsᴛᴇ ᴄʜᴀᴛ🚩*`, m)
 }
-
+handler.help = ['unbanchat'];
+handler.tags = ['owner'];
 handler.command = ['unbanchat','desbanearchat','desbanchat']
-handler.admin = true 
-handler.botAdmin = true
-handler.group = true
+handler.owner = true 
+handler.botAdmin = false
+handler.group = false
 
 export default handler
