47ad8fe3ecea5c6a315881f0316c06600b2aacc5
Elder504 <elderposadas028@gmail.com>
2025-04-03 06:53:34 -0600

    Actualizar Serbot_serbot.js

--- plugins/Serbot_serbot.js
+++ plugins/Serbot_serbot.js
@@ -268,7 +268,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363318758721861@newsletter', 
-                newsletterName: 'ᥴᥲᥒᥲᥣ ძᥱ іᥲᥒᥲᥣᥱȷᥲᥒძr᥆᥆k15᥊', 
+                newsletterName: '', 
                 serverMessageId: -1
               }
             }
@@ -284,7 +284,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363318758721861@newsletter', 
-                newsletterName: 'ᥴᥲᥒᥲᥣ ძᥱ іᥲᥒᥲᥣᥱȷᥲᥒძr᥆᥆k15᥊',
+                newsletterName: '᥊',
                 serverMessageId: -1
               }
             }
@@ -302,7 +302,7 @@ let handler = async (msg, { conn, args, usedPrefix, command, isOwner }) => {
               isForwarded: true,
               forwardedNewsletterMessageInfo: {
                 newsletterJid: '120363318758721861@newsletter', 
-                newsletterName: 'ᥴᥲᥒᥲᥣ ძᥱ іᥲᥒᥲᥣᥱȷᥲᥒძr᥆᥆k15᥊', 
+                newsletterName: '', 
                 serverMessageId: -1
               }
             }
