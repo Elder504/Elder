1f5d4b49e3fea236ec9e15d0af3b8760907d2dcf
Elder504 <elderposadas028@gmail.com>
2025-04-07 08:57:13 -0600

    Actualizar config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -12,7 +12,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   if (chat.welcome && m.messageStubType == 27) {
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 1200, height: 700, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .resize({ width: 1200, height: 1200, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
         .toBuffer();
 
       await conn.sendMessage(m.chat, { image: extendedImage, caption: '' });
