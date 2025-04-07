50cd856dac59d4608f00cb206c1067a6cdcd3ab8
Elder504 <elderposadas028@gmail.com>
2025-04-07 09:00:05 -0600

    Actualizar config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -10,15 +10,24 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat];
 
   if (chat.welcome && m.messageStubType == 27) {
+    let welcome = `
+      🌟 Bienvenido/a al grupo ${groupMetadata.subject}! 🌟
+      
+      👤 Usuario: @${m.messageStubParameters[0].split('@')[0]}
+      📜 Descripción: ${groupMetadata.desc || 'Sin descripción'}
+      
+      ¡Esperamos que disfrutes tu estadía! 😊
+    `;
+
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 1200, height: 1200, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .resize({ width: 1200, height: 1200, fit: 'inside', withoutEnlargement: true })
         .toBuffer();
 
-      await conn.sendMessage(m.chat, { image: extendedImage, caption: '' });
+      await conn.sendMessage(m.chat, { image: extendedImage, caption: welcome });
     } catch (error) {
       console.error('Error', error);
-      await conn.sendMessage(m.chat, { image: img, caption: '' });
+      await conn.sendMessage(m.chat, { image: img, caption: welcome });
     }
   }
 }
