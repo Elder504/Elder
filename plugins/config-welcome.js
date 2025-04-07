245af7929e0ab93ce1df860b10933a4a34c3e7b4
Elder504 <elderposadas028@gmail.com>
2025-04-07 08:42:13 -0600

    Actualizar config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -16,7 +16,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 1000, height: 1000, fit: 'cover' }) // Cambia 'contain' a 'cover'
+        .resize({ width: 1800, height: 1800, fit: 'cover' }) // Cambia 'contain' a 'cover'
         .toBuffer();
 
       await conn.sendMini(m.chat, redes, dev, welcome, extendedImage, extendedImage, redeshost);
