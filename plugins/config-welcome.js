b5b80354a4ea3e22e4c9218a664d8da5003809f7
Elder504 <elderposadas028@gmail.com>
2025-04-08 07:02:09 -0600

    Actualizar config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -10,19 +10,17 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat];
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = global.welcome
-      .replace('+tag', `@${m.messageStubParameters[0].split('@')[0]}`)
-      .replace('+description', groupMetadata.desc || 'Sin descripción');
+    let welcomeMessage = `Welcome to ${groupMetadata.subject}!`; // Ajusta el mensaje de bienvenida según tus necesidades
 
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 1800, height: 700, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .resize({ width: 1200, height: 500, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } }) // Ajusta estos valores
         .toBuffer();
 
-      await conn.sendMini(m.chat, redes, dev, welcome, extendedImage, extendedImage, redeshost);
+      await conn.sendMini(m.chat, redes, dev, welcomeMessage, extendedImage, extendedImage, redeshost);
     } catch (error) {
       console.error('Error', error);
-      await conn.sendMini(m.chat, redes, dev, welcome, img, img, redeshost);
+      await conn.sendMini(m.chat, redes, dev, welcomeMessage, img, img, redeshost);
     }
   }
 }
