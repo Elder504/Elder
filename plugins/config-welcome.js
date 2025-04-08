bfb842f407e7c44c47c0d8a9bf338fe6e16ed4d7
Elder504 <elderposadas028@gmail.com>
2025-04-08 06:58:59 -0600

    Actualizar config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -10,19 +10,20 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat];
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = global.welcome
-      .replace('+tag', `@${m.messageStubParameters[0].split('@')[0]}`)
-      .replace('+description', groupMetadata.desc || 'Sin descripción');
+    let welcomeText = `Welcome to ${groupMetadata.subject}!`;
+    let welcomeBody = `Hello @${m.messageStubParameters[0].split('@')[0]},\n\n${groupMetadata.desc || 'No description available.'}`;
+    
+    let welcomeMessage = `${welcomeText}\n\n${welcomeBody}`;
 
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 1200, height: 1200, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .resize({ width: 1200, height: 500, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } }) // Ajusta estos valores
         .toBuffer();
 
-      await conn.sendMini(m.chat, redes, dev, welcome, extendedImage, extendedImage, redeshost);
+      await conn.sendMini(m.chat, redes, dev, welcomeMessage, extendedImage, extendedImage, redeshost);
     } catch (error) {
       console.error('Error', error);
-      await conn.sendMini(m.chat, redes, dev, welcome, img, img, redeshost);
+      await conn.sendMini(m.chat, redes, dev, welcomeMessage, img, img, redeshost);
     }
   }
 }
