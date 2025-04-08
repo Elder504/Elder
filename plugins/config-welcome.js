12b5018d1e96d1053d7ce25c8a37e17b97889d85
Elder504 <elderposadas028@gmail.com>
2025-04-07 21:00:26 -0600

    Update config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -4,43 +4,25 @@ import sharp from 'sharp';
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-
-  let pp;
-  try {
-    pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image');
-  } catch (error) {
-    pp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60';
-  }
-
-  let img;
-  try {
-    img = await (await fetch(pp)).buffer();
-  } catch (error) {
-    console.error('Error al obtener la imagen del perfil:', error);
-    return;
-  }
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60');
+  let img = await (await fetch(pp)).buffer();
 
   let chat = global.db.data.chats[m.chat];
 
-  if (chat && chat.welcome && m.messageStubType == 27) {
-    let welcome = `
-      🌟 Bienvenido/a al grupo ${groupMetadata.subject}! 🌟
-      
-      👤 Usuario: @${m.messageStubParameters[0].split('@')[0]}
-      📜 Descripción: ${groupMetadata.desc || 'Sin descripción'}
-      
-      ¡Esperamos que disfrutes tu estadía! 😊
-    `;
+  if (chat.welcome && m.messageStubType == 27) {
+    let welcome = global.welcome
+      .replace('+tag', `@${m.messageStubParameters[0].split('@')[0]}`)
+      .replace('+description', groupMetadata.desc || 'Sin descripción');
 
     try {
       const extendedImage = await sharp(img)
-        .resize({ width: 1200, height: 1200, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .resize({ width: 1800, height: 700, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
         .toBuffer();
 
-      await conn.sendMessage(m.chat, { image: extendedImage, caption: welcome });
+      await conn.sendMini(m.chat, redes, dev, welcome, extendedImage, extendedImage, redeshost);
     } catch (error) {
-      console.error('Error al procesar la imagen:', error);
-      await conn.sendMessage(m.chat, { image: img, caption: welcome });
+      console.error('Error', error);
+      await conn.sendMini(m.chat, redes, dev, welcome, img, img, redeshost);
     }
   }
 }
