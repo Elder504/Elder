ff892071ca0b4b74b5cb3d9597a2fbc93514a080
Elder504 <elderposadas028@gmail.com>
2025-04-08 06:43:14 -0600

    Actualizar config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const app = express();
+import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
+import sharp from 'sharp';
 
-// Middleware para parsear JSON
-app.use(express.json());
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60');
+  let img = await (await fetch(pp)).buffer();
 
-// Ruta de bienvenida sin test
-app.get('/welcome', (req, res) => {
-  const welcomeMessage = {
-    message: 'Welcome to our application!',
-    body: 'This is the body of the welcome message.'
-  };
-  res.json(welcomeMessage);
-});
+  let chat = global.db.data.chats[m.chat];
 
-// Inicia el servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  if (chat.welcome && m.messageStubType == 27) {
+    let welcome = global.welcome
+      .replace('+tag', `@${m.messageStubParameters[0].split('@')[0]}`)
+      .replace('+description', groupMetadata.desc || 'Sin descripción');
+
+    try {
+      const extendedImage = await sharp(img)
+        .resize({ width: 1800, height: 700, fit: 'contain', background: { r: 0, g: 0, b: 0, alpha: 0 } })
+        .toBuffer();
+
+      await conn.sendMini(m.chat, redes, dev, welcome, extendedImage, extendedImage, redeshost);
+    } catch (error) {
+      console.error('Error', error);
+      await conn.sendMini(m.chat, redes, dev, welcome, img, img, redeshost);
+    }
+  }
+}
