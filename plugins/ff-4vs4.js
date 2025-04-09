d4a123b8470b72995604cf6c5fe042f6f944d6c7
Elder504 <elderposadas028@gmail.com>
2025-04-09 11:53:40 -0600

    Actualizar ff-4vs4.js

--- plugins/ff-4vs4.js
+++ plugins/ff-4vs4.js
@@ -2,10 +2,10 @@
  
 let handler = async(m, { conn }) => {
 
-let vs = `*🩵 4 VS 4 - FREE FIRE*\n\n`
-    vs += `🛡️ PARTICIPANTES:\n`
+let vs = `*𝗩𝗔𝗠𝗢𝗦 𝗔 𝗖𝗢𝗠𝗡̃𝗘*\n\n`
+    vs += `𝐄𝐒𝐂𝐔𝐀𝐃𝐑𝐀:\n`
     vs += `👑 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n`
-    vs += `💫 *SUPLENTES*:\n`
+    vs += `💫 *𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒*:\n`
     vs += `🥷🏻 ┇\n🥷🏻 ┇\n`
     vs += `🍨 ${dev}`
 
