7321c29c58e85615c92525f94183d53b4943ca93
Elder504 <elderposadas028@gmail.com>
2025-04-09 22:01:41 -0600

    Actualizar ff-4vs4.js

--- plugins/ff-4vs4.js
+++ plugins/ff-4vs4.js
@@ -1,4 +1,19 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!m.isGroup) return; // Si no es un grupo, no hacer nada
-  await m.reply('El comando ha sido ejecutado correctamente.'); // Mensaje de prueba
-};
+// By WillZek Para CrowBot
+ 
+let handler = async(m, { conn }) => {
+
+let vs = `🗣️ 𝗩𝗔𝗠𝗢𝗦 𝗔 𝗖𝗢𝗠𝗣𝗘\n\n`
+    vs += `💫 *𝐄𝐒𝐂𝐔𝐀𝐃𝐑𝐀*:\n`
+    vs += `👑 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n🥷🏻 ┇\n`
+    vs += `💫 *𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒*:\n`
+    vs += `🥷🏻 ┇\n🥷🏻 ┇\n`
+    vs += `🍨 ${dev}`
+
+let vsimg = 'https://files.catbox.moe/ev7gjp.jpg';
+
+conn.sendMessage(m.chat, { image: { url: vsimg }, caption: vs }, { quoted: m });
+}
+
+handler.command = ['4vs4'];
+
+export default handler
